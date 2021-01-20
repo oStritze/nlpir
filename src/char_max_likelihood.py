2e9d2fa18bb3926e50ff2fc1c6d3d65f4eb770d6
oStritze <oliver.stritzel@googlemail.com>
2021-01-20 15:40:58 +0100

    applied changes to char_max_likelihood.py

--- src/char_max_likelihood.py
+++ src/char_max_likelihood.py
@@ -12,16 +12,20 @@ import utils
 from collections import *
 import random
 
-
 def concat_speeches(president:str, order=4):
     fpath = "../data/" + president +"/"
     all_speeches = ""
     for file in os.listdir(fpath):
-        s = open(fpath+file, "r").read()
-        all_speeches += s
+        if file == ".ipynb_checkpoints":
+            next
+        else:
+            s = open(fpath+file, "r").read()
+            #s = s.replace("\n\n", "\n")
+            pad = "~" * order
+            all_speeches += pad + s + "\n" 
+            #all_speeches += s
     return all_speeches
-
-
+    
 def train_char_lm_on_single_speech(fname, order=4):
     data = open(fname, "r").read()
     lm = defaultdict(Counter)
@@ -30,40 +34,37 @@ def train_char_lm_on_single_speech(fname, order=4):
     for i in range(len(data)-order):
         history, char = data[i:i+order], data[i+order]
         lm[history][char]+=1
-
     def normalize(counter):
         s = float(sum(counter.values()))
         return [(c,cnt/s) for c,cnt in counter.items()]
-
     outlm = {hist:normalize(chars) for hist, chars in lm.items()}
     return outlm
 
 def train_char_lm(president="reagan", order=4):
     data = concat_speeches(president, order)
     lm = defaultdict(Counter)
-    pad = "~" * order
-    data = pad + data
+    #pad = "~" * order
+    #data = pad + data
     for i in range(len(data)-order):
         history, char = data[i:i+order], data[i+order]
         lm[history][char]+=1
-
     def normalize(counter):
         s = float(sum(counter.values()))
         return [(c,cnt/s) for c,cnt in counter.items()]
-
     outlm = {hist:normalize(chars) for hist, chars in lm.items()}
     return outlm
-
 
 def generate_letter(lm, history, order):
     history = history[-order:]
     dist = lm[history]
     x = random.random()
+    #print(history, dist, x)
     for c,v in dist:
         x = x - v
-        if x <= 0: return c
-
-
+        # do not predict a "~" since this would mix different starts into one speech - this could be used for early-ending of the speech though
+        if x <= 0 and c != "~": 
+            return c
+		
 def generate_text(lm, order, nletters=1000):
     history = "~" * order
     out = []
@@ -71,4 +72,12 @@ def generate_text(lm, order, nletters=1000):
         c = generate_letter(lm, history, order)
         history = history[-order:] + c
         out.append(c)
-    return "".join(out)
+    outstring = "".join(out)
+    ## kill last unfinished word
+    cnt = len(outstring) - 1
+    while cnt>=0:
+        if outstring[cnt] in [" ", ",", "."]:
+            break
+        else:    
+            cnt = cnt -1
+    return outstring
